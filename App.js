8fbdfcaafb6b66a53d06d33ad9335d8b6c0bfe5f
Dali-Hili <you@example.com>
2021-04-30 02:44:59 +0200

    fixing the order

--- App.js
+++ App.js
@@ -11,39 +11,39 @@ import LoginGoogle from './components/GoogleLogin';
 import Profile from './components/Profile'
 import Settings from './components/Settings'
 // import Maps from './components/Maps'
-// import Order from  '../client-side/components/src/app';
+import Order from  '../client-side/components/src/Parks';
 
 export default function App() {
   const Stack = createStackNavigator();
   return (
- <NavigationContainer >
-    <Stack.Navigator initialRouteName="Home" 
-   screenOptions={{
-    headerTintColor: 'white',
-    headerStyle: { backgroundColor: '#212121' },
-  }}>
-      <Stack.Screen 
-       name="Home"
-       component={HomeScreen}
-       style={{backgroundColor: 'black'}} 
-      />
-      <Stack.Screen
-        name="Signup"
-        component={Signup}
-      />
-      <Stack.Screen
-        name="Signin"
-        component={Signin}
-      />
-      <Stack.Screen
-        name="TabNavigator"
-        component={TabNavigator}
-      />
-    </Stack.Navigator>
-  </NavigationContainer>
+//  <NavigationContainer >
+//     <Stack.Navigator initialRouteName="Home" 
+//    screenOptions={{
+//     headerTintColor: 'white',
+//     headerStyle: { backgroundColor: '#212121' },
+//   }}>
+//       <Stack.Screen 
+//        name="Home"
+//        component={HomeScreen}
+//        style={{backgroundColor: 'black'}} 
+//       />
+//       <Stack.Screen
+//         name="Signup"
+//         component={Signup}
+//       />
+//       <Stack.Screen
+//         name="Signin"
+//         component={Signin}
+//       />
+//       <Stack.Screen
+//         name="TabNavigator"
+//         component={TabNavigator}
+//       />
+//     </Stack.Navigator>
+//   </NavigationContainer>
   // <TabNavigator/>
     //  <Settings/>
-    // <Order/>
+    <Order/>
   // <LoginGoogle/>
   );
 }
--- components/src/app.js
+++ components/src/app.js
@@ -1,30 +1,44 @@
-import React, { Component } from 'react';
-import {
-  Navigator,
-} from 'react-native';
-import Parks from './Parks.js';
-const RouteMapper = (route, navigator) => {
-  if (route.name === 'movies') {
-    return (
-      <Parks navigator={navigator} />
-    );
-  } else if (route.name === 'confirmation') {
-    return (
-      <Confirmation code={route.code} navigator={navigator} />
-    );
-  }
-};
+// import React, { Component } from 'react';
+// import {Navigator} from 'react-native';
+// import Parks from './Parks.js';
+// const RouteMapper = (route, navigator) => {
+//   if (route.name === 'movies') {
+//     return (
+//       <Parks navigator={navigator} />
+//     );
+//   } else if (route.name === 'confirmation') {
+//     return (
+//       <Confirmation code={route.code} navigator={navigator} />
+//     );
+//   }
+// };
+
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <Navigator
+//         // Default to parks route
+//         initialRoute={{ name: 'parks' }}
+//         // Use FloatFromBottom transition between screens
+//         configureScene={(route, routeStack) => Navigator.SceneConfigs.FloatFromBottom}
+//         // Pass a route mapper functions
+//         //gaston
+//         renderScene={RouteMapper}
+//       />
+//     );
+//   }
+// }
+
 
 export default class App extends Component {
   render() {
     return (
       <Navigator
-        // Default to parks route
-        initialRoute={{ name: 'parks' }}
+        // Default to movies route
+        initialRoute={{ name: 'movies' }}
         // Use FloatFromBottom transition between screens
         configureScene={(route, routeStack) => Navigator.SceneConfigs.FloatFromBottom}
         // Pass a route mapper functions
-        //gaston
         renderScene={RouteMapper}
       />
     );
